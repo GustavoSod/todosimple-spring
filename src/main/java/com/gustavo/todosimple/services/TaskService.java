b4551e8d28a781b80abcbd12aa404aa20af7b35a
Gustavo Sodré <gustavo.sodre.carreira.rosa@gmail.com>
2024-03-25 09:17:14 -0300

    Create task controller

--- src/main/java/com/gustavo/todosimple/services/TaskService.java
+++ src/main/java/com/gustavo/todosimple/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.gustavo.todosimple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,11 @@ public class TaskService {
         Optional<Task> task = this.taskRepository.findById(id);
         return task.orElseThrow(() -> new RuntimeException("task not found! Id: " + id + "tipo: " + Task.class.getName()));
     } 
+
+    public List<Task> findAllByUserId (Long userId){
+        List<Task> tasks = this.taskRepository.findByUserId(userId);
+        return tasks;
+    }
 
     @Transactional
     public Task create (Task obj){
